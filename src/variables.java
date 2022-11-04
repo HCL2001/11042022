b3cb355febb4eed27730615536a2edba9c5b013b
Long Hoang <longhoang12212@gmai.com>
2022-11-04 10:12:04 +0700

    variables.html

---
+++ src/variables.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class variables {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int i = 10;
+        float f=  20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Ha Noi";
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+    }
+}
