86f0ef44d25b957e8a0cf476026516b31e251bc8
HCL <98075045+HCL2001@users.noreply.github.com>
2022-11-04 11:03:09 +0700

    loiChao

---
+++ src/loiChao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class loiChao {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap vao ten cua ban: ");
+        String name = sc.next();
+        System.out.println("Hello: " + name);
+    }
+}
