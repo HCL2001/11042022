35be5b39deadc1b271306c7093714c145b39baa3
Long Hoang <longhoang12212@gmai.com>
2022-11-04 10:07:30 +0700

    Demo

---
+++ src/systemTime.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class systemTime {
+    public static void main(String[] arg){
+        Date now = new Date();
+        System.out.println("Ngay hom nay la: " + now);
+    }
+}
